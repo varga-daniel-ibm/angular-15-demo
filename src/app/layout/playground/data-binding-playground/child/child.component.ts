93efb2ecfa65309835bc736cf18954c00f99c2b4
Daniel <vargadaniel28@gmail.com>
2023-07-11 18:56:51 +0300

    Added data binding examples

--- src/app/layout/playground/data-binding-playground/child/child.component.ts
+++ src/app/layout/playground/data-binding-playground/child/child.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit{
+  @Input() favoriteCity: string = 'test';
+  @Output() onChangeFavoriteCountry: EventEmitter<string> = new EventEmitter();
+
+  ngOnInit(): void {}
+
+  emitFavoriteCountry(): void {
+    this.onChangeFavoriteCountry.emit('Thailand');
+  }
 
 }
--- src/app/layout/playground/data-binding-playground/data-binding-playground.component.ts
+++ src/app/layout/playground/data-binding-playground/data-binding-playground.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-binding-playground.component.scss']
 })
 export class DataBindingPlaygroundComponent {
+  favoriteFood: string = 'Sushi';
+  favoriteCity: string = 'Barcelona';
 
+  changeFavoriteFood(): void {
+    this.favoriteFood = this.favoriteFood === 'Sushi' ? 'Pizza' : 'Sushi';
+  }
+
+  onFavoriteFoodInputChange(event: Event): void {
+    this.favoriteFood = (event.target as HTMLInputElement).value;
+  }
+
+  onChangeFavoriteCountry(favoriteCountry: string): void {
+    console.log("Favorite Country:", favoriteCountry)
+  }
 }
